695ff11e4dca730205bbd02c4d1f62d8619322cb
Julio <julio.nepomuceno@eurekalabs.com.br>
2024-02-05 21:51:26 -0300

    feature: starting to imlement board and piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import boardGame.Board;
+
 public class Program {
     public static void main(String[] args) {
+        Board board = new Board(8,8);
         System.out.println("Hello world");
 
     }
---
+++ src/boardGame/Piece.java
@@ -0,0 +1,15 @@
+package boardGame;
+
+public class Piece {
+    /* By default it is null */
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+}
